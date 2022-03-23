540a815be0858fe45afb885c5682f1153ad98b64
Lasse Stewart <taketsuki@protonmail.com>
2022-03-23 13:07:05 -0300

    [add] entities list

--- utils/entities_list.py
+++ utils/entities_list.py
@@ -2,11 +2,102 @@
 # @Author: Wenwen Yu
 # @Created Time: 7/8/2020 9:34 PM
 
+#Entities_list = [
+#    "company",
+#    "address",
+#    "date",
+#    "total"
+#]
+
+#Entities_list = [
+#	"nome",
+#	"cpf",
+#        "meta",
+#        "info-datanasc",
+#        "data-nascimento",
+#        "info-nome",
+#        "info-emissao",
+#        "data-emissao",
+
+#]
+
+#Entities_list = [
+#   "info-nome",
+#   "nome",
+#   "info-pais",
+#   "pais",
+#   "rg",
+#   "cpf",
+#   "info-cpf",
+#   "info-rg",
+#   "data-nascimento",
+#   "info-datanasc",
+#   "info-perm",
+#   "info-datavalidade",
+#   "data-validade",
+#   "info-hab",
+#   "data-hab",
+#   "info-acc",
+#   "info-obs",
+#   "info-docorigem",
+#   "meta",
+#   "ass-titular",
+#   "info-asstitular",
+#   "ass-diretor",
+#   "info-assdiretor",
+#   "data-hab",
+#   "obs"
+#]
+
 Entities_list = [
-    "company",
-    "address",
-    "date",
-    "total"
+    "eh-idoso",
+    "info-rg",
+    "info-nome",
+    "rg",
+    "meta",
+    "estado-emissao",
+    "info-naturalidade",
+    "info-dataexp",
+    "info-datanasc",
+    "data-nascimento",
+    "data-expedicao",
+    "info-filiacao",
+    "org",
+    "filiacao",
+    "nome",
+    "info-cpf",
+    "cpf",
+    "naturalidade",
+    "info-asstitular",
+    "ass-titular",
+    "info-assdiretor",
+    "ass-diretor",
+    "info-coordenador",
+    "coordenador",
+    "via",
+    "info-docorigem",
+    "doc-origem",
+    "info-perm",
+    "info-acc",
+    "info-local",
+    "local",
+    "info-dataemissao",
+    "data-emissao",
+    "info-obs",
+    "obs",
+    "info-primeirahab",
+    "primeira-hab",
+    "info-validade",
+    "validade",
+    "info-cathab",
+    "cat-hab",
+    "info-renach",
+    "renach",
+    "uf",
+    "info-doc",
+    "doc-id",
+    "org-emissor",
+    "unknown"
 ]
 
 # Entities_list = [
