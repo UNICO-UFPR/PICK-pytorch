3e12e4432e4a984511367d0f7b41b5f5bfce2bda
Jose Pedro Silveira Martinez <jpsmartinez@inf.ufrgs.br>
2022-05-03 19:15:59 +0000

    UPD: unico's entities

--- utils/entities_list.py
+++ utils/entities_list.py
@@ -1,112 +1,13 @@
 # -*- coding: utf-8 -*-
-# @Author: Wenwen Yu
-# @Created Time: 7/8/2020 9:34 PM
-
-#Entities_list = [
-#    "company",
-#    "address",
-#    "date",
-#    "total"
-#]
-
-#Entities_list = [
-#	"nome",
-#	"cpf",
-#        "meta",
-#        "info-datanasc",
-#        "data-nascimento",
-#        "info-nome",
-#        "info-emissao",
-#        "data-emissao",
-
-#]
-
-#Entities_list = [
-#   "info-nome",
-#   "nome",
-#   "info-pais",
-#   "pais",
-#   "rg",
-#   "cpf",
-#   "info-cpf",
-#   "info-rg",
-#   "data-nascimento",
-#   "info-datanasc",
-#   "info-perm",
-#   "info-datavalidade",
-#   "data-validade",
-#   "info-hab",
-#   "data-hab",
-#   "info-acc",
-#   "info-obs",
-#   "info-docorigem",
-#   "meta",
-#   "ass-titular",
-#   "info-asstitular",
-#   "ass-diretor",
-#   "info-assdiretor",
-#   "data-hab",
-#   "obs"
-#]
-
 Entities_list = [
-    "eh-idoso",
-    "info-rg",
-    "info-nome",
-    "rg",
-    "meta",
-    "estado-emissao",
-    "info-naturalidade",
-    "info-dataexp",
-    "info-datanasc",
-    "data-nascimento",
-    "data-expedicao",
-    "info-filiacao",
-    "org",
-    "filiacao",
-    "nome",
-    "info-cpf",
-    "cpf",
-    "naturalidade",
-    "info-asstitular",
-    "ass-titular",
-    "info-assdiretor",
-    "ass-diretor",
-    "info-coordenador",
-    "coordenador",
-    "via",
-    "info-docorigem",
-    "doc-origem",
-    "info-perm",
-    "info-acc",
-    "info-local",
-    "local",
-    "info-dataemissao",
-    "data-emissao",
-    "info-obs",
-    "obs",
-    "info-primeirahab",
-    "primeira-hab",
-    "info-validade",
-    "validade",
-    "info-cathab",
-    "cat-hab",
-    "info-renach",
-    "renach",
-    "uf",
-    "info-doc",
-    "doc-id",
-    "org-emissor",
-    "unknown"
+    'pai',
+    'doc_origem',
+    'data_nascimento',
+    'nome',
+    'unknown',
+    'data_expedicao',
+    'naturalidade',
+    'cpf',
+    'rg',
+    'mae'
 ]
-
-# Entities_list = [
-#     "ticket_num",
-#     "starting_station",
-#     "train_num",
-#     "destination_station",
-#     "date",
-#     "ticket_rates",
-#     "seat_category",
-#     "name"
-# ]
